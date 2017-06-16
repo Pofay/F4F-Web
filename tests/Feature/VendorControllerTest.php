2bc1d18f7299d270ed2a9c4a3541a856885bc80e
Gian Carlo I. Gilos <gianCarlo.gilos@outlook.com>
2017-06-16 19:46:24 +0800

    Removed old tests since they tested the wrong implementation

--- tests/Feature/VendorControllerTest.php
+++ tests/Feature/VendorControllerTest.php
@@ -23,56 +23,5 @@ class VendorControllerTest extends TestCase
         $response->assertStatus(200);
     }
 
-	public function testGetWithIdReturnsCorrespondingVendor()
-	{
-		$vendor = factory(\App\Vendor::class)->create([
-			'name' => 'Pofay'
-		]);
 
-		$item = factory(\App\Item::class)->create([
-			'vendor_id' => $vendor->id,
-			'name' => 'Potato',
-			'quantity' => 6,
-			'price' => 4.25
-		]);
-	
-		$expected = [
-			'vendor_name' => 'Pofay',
-			'items' => 
-			[
-				'vendor_id' => $vendor->id,
-				'id' => $item->id,
-				'name' => 'Potato',
-				'quantity' => 6,
-				'price' => 4.25
-			]
-		];
-
-		$response = $this->get('/api/v1/vendor/'.$vendor->id);
-
-		$response->assertJson($expected);
-	}
-
-	public function testGetReturnsAListOfAllVendors() 
-	{
-        $vendor = factory(\App\Vendor::class)->create([
-			'name' => 'Pofay'
-		]);
-
-
-		$expected = [
-			'vendors' =>
-		   	[
-				[
-				    'id' => $vendor->id,
-				    'name' => $vendor->name
-				]
-			]
-		];
-
-
-		$response = $this->get('api/v1/vendor');
-
-		$response->assertJson($expected);
-	}
 }
