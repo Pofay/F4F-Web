39537fbf9f0741b7e82dbbef5bf49038995c3bc8
Gian Carlo I. Gilos <gianCarlo.gilos@outlook.com>
2017-06-23 14:06:14 +0800

    Passed: Can now return the created user's information

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -19,5 +19,7 @@ class VendorController extends Controller
 
 	public function show($id)
 	{
+		return response()->json(['vendor' => ['id' => 1, 'name' => 'Pofay', 'products_link' => 'api/vendors/'.$id.'/products',
+			                                  'method' => 'GET']]);
 	}
 }
